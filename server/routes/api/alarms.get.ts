fabdcaf65f310b44df57ea3347db1f8f402f1bbb
[dyad] <git@dyad.sh>
2026-05-13 22:45:14 +0200

    [dyad] (16 files changed)

--- server/routes/api/alarms.get.ts
+++
@@ -1,6 +0,0 @@
-import { defineEventHandler } from 'nitropack/runtime';
-import db from '../../utils/db';
-
-export default defineEventHandler(async (event) => {
-  return db.prepare('SELECT * FROM alarms ORDER BY time ASC').all();
-});
--- server/routes/api/todos/[id].delete.ts
+++
@@ -1,8 +0,0 @@
-import { defineEventHandler, getRouterParam } from 'nitropack/runtime';
-import db from '../../utils/db';
-
-export default defineEventHandler(async (event) => {
-  const id = getRouterParam(event, 'id');
-  db.prepare('DELETE FROM todos WHERE id = ?').run(id);
-  return { success: true };
-});
